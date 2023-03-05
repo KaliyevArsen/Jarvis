c6924fa6c6fbb608ede6efb340200314b8936349
Kaliyev Arsen <121351989+programer3001@users.noreply.github.com>
2023-03-05 16:10:08 +0600

    Update skills.py

--- skills.py
+++ skills.py
@@ -104,7 +104,8 @@ def sleep():
 def weather():
     api_key = "your API key"
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    complete_url = base_url + "appid=" + api_key + "&q=" + "Astana"
+    city = "Astana"
+    complete_url = f"{base_url}appid={api_key}&q={city}"
     response = requests.get(complete_url)
     x = response.json()
     if x["cod"] != "404":
